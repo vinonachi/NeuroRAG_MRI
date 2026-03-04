db50722668dbc2d128029fa12f058204a849c7f6
Vinothini N <134153950+vinonachi@users.noreply.github.com>
2026-03-04 23:23:55 +0530

    Refactor simulate_low_resolution to use OpenCV
    Updated the simulate_low_resolution function to use OpenCV for resizing instead of skimage.

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,9 +1,12 @@
+import cv2
 import numpy as np
-from skimage.transform import resize
 
 
-def simulate_low_resolution(image, scale=2):
+def simulate_low_resolution(image):
+
     h, w = image.shape
-    small = resize(image, (h // scale, w // scale), anti_aliasing=True)
-    restored = resize(small, (h, w), anti_aliasing=True)
-    return restored
+
+    small = cv2.resize(image, (w//2, h//2))
+    upscaled = cv2.resize(small, (w, h))
+
+    return upscaled
