567dde58593413d0020400813ca60ac2ee12d8ea
Vinothini N <134153950+vinonachi@users.noreply.github.com>
2026-03-04 23:26:55 +0530

    Refactor image resizing with interpolation

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,12 +1,15 @@
-import cv2
 import numpy as np
+import cv2
 
 
 def simulate_low_resolution(image):
 
     h, w = image.shape
 
-    small = cv2.resize(image, (w//2, h//2))
-    upscaled = cv2.resize(small, (w, h))
+    # Downscale
+    small = cv2.resize(image, (w//2, h//2), interpolation=cv2.INTER_LINEAR)
+
+    # Upscale
+    upscaled = cv2.resize(small, (w, h), interpolation=cv2.INTER_LINEAR)
 
     return upscaled
